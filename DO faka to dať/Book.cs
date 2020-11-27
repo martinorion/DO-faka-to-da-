ff4ac55ab2c5abf69b48aeb7bdde32ff667e28c5
Kristián Jakubik <martinkiko18@gmail.com>
2020-11-27 12:32:50 +0100

    Add project files.

---
+++ DO faka to dať/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DO_faka_to_dať
+{
+    class Book
+    {
+        public string title;
+        private int pages;
+
+        public Book(string title, int pages)
+        {
+            this.title = title;
+            this.pages = pages;
+        }
+    }
+}
---
+++ DO faka to dať/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DO_faka_to_dať
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book LOTR = new Book("Rozprávka", 60);
+
+            Console.WriteLine(LOTR.title);
+            
+        }
+    }
+}
